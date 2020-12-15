42ab1d3f4d3cc118ee4016d3be9d28af538746c0
sholeh007 <sholeh09@outlook.com>
2020-12-15 11:33:31 +0900

    menambah fitur filter

--- src/MeetUp/MeetGrid.svelte
+++ src/MeetUp/MeetGrid.svelte
@@ -1,26 +1,42 @@
 <script>
   import MeetItem from "./MeetItem.svelte";
+  import MeetupFilter from "../MeetUp/MeetupFilter.svelte";
 
   export let meets;
+  let favOnly = false;
+
+  $: filterSet = favOnly ? meets.filter((item) => item.isFavorite) : meets;
+
+  const setFilter = (event) => {
+    favOnly = event.detail === 1;
+  };
 </script>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  #meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
 </style>
 
-<section>
-  {#each meets as meet}
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} />
+</section>
+
+<section id="meetups">
+  {#each filterSet as meet}
     <MeetItem
       id={meet.id}
       title={meet.title}
