e362cb225ba81778311d11d440012ca133056fdc
sholeh007 <sholeh09@outlook.com>
2020-12-19 23:00:20 +0900

    menambah method fetching data di file meetApi

--- src/api/meetApi.js
+++ src/api/meetApi.js
@@ -1,4 +1,13 @@
 const dataApi = {
+  getData: async (url) => {
+    const gettingData = await fetch(url);
+    const dataMeet = await gettingData.json();
+
+    if (!gettingData.ok) {
+      throw new Error('Failed to get data!');
+    };
+    return dataMeet;
+  },
   postData: async (url, setting) => {
     const sendData = await fetch(url, setting);
     const dataMeet = await sendData.json();
